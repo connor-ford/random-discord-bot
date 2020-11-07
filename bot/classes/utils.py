a935e3522e2446b6d92017619b62d6ba0e4c2526
Connor Ford <connor.ford2022@gmail.com>
2020-11-07 14:07:54 -0600

    Update prefix to new structure

--- bot/classes/utils.py
+++ bot/classes/utils.py
@@ -10,7 +10,7 @@ class List():
         message['message'] = 'List of commands and their descriptions:\n```'
 
         for command in commands:
-            message['message'] += f'\n{guild_data["prefix"]}{command["command"]} - ' + \
+            message['message'] += f'\n{guild_data["general"]["prefix"]}{command["command"]} - ' + \
                 (f'{command["description"]}' if 'description' in command else '')
         message['message'] += '\n```'
 
@@ -48,12 +48,12 @@ class Usage():
                 found_command = command
         
         if not found_command:
-            return {'error': 'USAGE', 'message': f'Command not recognized. Please use {guild_data["prefix"]}list to see a full list of commands.'}
+            return {'error': 'USAGE', 'message': f'Command not recognized. Please use {guild_data["general"]["prefix"]}list to see a full list of commands.'}
 
         message = {}
         usages = found_command['usage'] if type(found_command['usage']) is list else [
             found_command['usage']]
         message['message'] = f'Usage:'
         for usage in usages:
-            message['message'] += f'\n`{guild_data["prefix"]}{found_command["command"]} {usage}`'
+            message['message'] += f'\n`{guild_data["general"]["prefix"]}{found_command["command"]} {usage}`'
         return message

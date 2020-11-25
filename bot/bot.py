c7b4fad1432b177fff40d325d6575617df6eff46
Connor <connor.ford2022@gmail.com>
2020-11-25 11:15:40 -0600

    Add mcuser command

--- bot/bot.py
+++ bot/bot.py
@@ -6,7 +6,7 @@ import sys
 
 import discord
 
-from methods.api import cat_api, dog_api, joke_api
+from methods.api import cat_api, dog_api, joke_api, mc_username_finder
 from methods.utils import list_commands, change_prefix, get_usage
 from config import TOKEN
 
--- bot/methods/api.py
+++ bot/methods/api.py
@@ -1,7 +1,10 @@
 import random
+from datetime import datetime
+from uuid import UUID
 
 import requests
 from discord import Embed
+
 
 def cat_api(params=None, guild_data=None):
     if not params:
@@ -154,6 +157,7 @@ def cat_api(params=None, guild_data=None):
 
     return message
 
+
 def dog_api(params=None, guild_data=None):
     if not params:
         return {'error': 'USAGE'}
@@ -216,6 +220,7 @@ def dog_api(params=None, guild_data=None):
 
     return message
 
+
 def joke_api(params=None, guild_data=None):
     if not params:
         return {'error': 'USAGE'}
@@ -258,4 +263,62 @@ def joke_api(params=None, guild_data=None):
     else:
         message['message'] = f'{response.json()["joke"]}'
 
+    return message
+
+
+def mc_username_finder(params=None, guild_data=None):
+    if not params or len(params.split()) > 1:
+        return {'error': 'USAGE', 'message': 'Please supply a valid username or UUID.'}
+
+    mc_uuid = None
+    mc_username = ""
+
+    # Check if UUID
+    try:
+        mc_uuid = str(UUID(params.split()[0], version=4))
+    except ValueError:
+        mc_username = params.split()[0]
+
+    # If not UUID, get UUID
+    if mc_username:
+        response = requests.get(
+            url=f'https://api.mojang.com/users/profiles/minecraft/{mc_username}')
+        if response.status_code == 204:  # No content = no information
+            return {'message': 'The supplied username could not be found.'}
+        if response.status_code != 200:
+            return {'error': 'API', 'message': response.text}
+
+        mc_uuid = str(UUID(response.json()['id'], version=4))
+
+    # Get previous names
+    response = requests.get(
+        url=f'https://api.mojang.com/user/profiles/{mc_uuid}/names')
+    if response.status_code == 204:  # No content = no information
+        return {'message': 'The supplied UUID could not be found.'}
+    if response.status_code != 200:
+        return {'error': 'API', 'message': response.text}
+
+    mc_prev_names = response.json()
+    # Proper capitalization of name due to prior lower() of params
+    mc_username = mc_prev_names[-1]['name']
+
+    message = {}
+    embed = Embed(
+        title=mc_username,
+        colour=000000,
+        description=mc_uuid
+    )
+
+    # Image of player skin's head
+    mc_image_url = f'https://crafatar.com/avatars/{mc_uuid}'
+    embed.set_image(url=mc_image_url)
+
+    # Format previous name timestamps
+    mc_prev_names_str = ""
+    for prev_name in mc_prev_names:
+        mc_prev_names_str += f'{prev_name["name"]}: ' + (datetime.utcfromtimestamp(
+            prev_name['changedToAt'] / 1000).strftime('%e %B, %Y %I:%M %p') if 'changedToAt' in prev_name else 'NA') + '\n'
+    embed.add_field(name='Previous Names', value=mc_prev_names_str)
+
+    message['embed'] = embed
     return message

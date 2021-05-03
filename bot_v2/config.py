cd80e3c5a067c07d8c8a2ef8e61677f35f2906ca
Connor Ford <connor.ford2022@gmail.com>
2021-05-03 12:35:22 -0500

    Delete bot_v2 directory

--- bot_v2/config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = "<Your Bot Token Here>"
-
-LOG_LEVEL_STDOUT = "DEBUG"  # Console log level
-LOG_LEVEL_FILE = "INFO"  # File log level
-
-LOG_CACHE = True  # Cache logs toggle
-LOG_KEYWORDS = True  # Keyword logs toggle
